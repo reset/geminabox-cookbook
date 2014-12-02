1b97bc9acdbb0b07e2b8d148ac27282f23ca8ea4
Tom Scott <tscott@telvue.com>
2014-12-02 12:19:04 -0500

    Add configuration settings to rackup file
    We needed to configure proxying to rubygems.org and allowing for remote
    failure, so we added the ability to configure the Geminabox app using
    config.ru.

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,9 +17,11 @@
 # limitations under the License.
 #
 
-default[:gem_server][:ruby_version] = "1.9.3-p448"
-default[:gem_server][:version]      = nil
-default[:gem_server][:port]         = 9010
-default[:gem_server][:host]         = node[:fqdn]
-default[:gem_server][:home_path]    = "/opt/geminabox"
-default[:gem_server][:data_path]    = "#{node[:gem_server][:home_path]}/data"
+default[:gem_server][:ruby_version]         = "1.9.3-p448"
+default[:gem_server][:version]              = nil
+default[:gem_server][:port]                 = 9010
+default[:gem_server][:host]                 = node[:fqdn]
+default[:gem_server][:home_path]            = "/opt/geminabox"
+default[:gem_server][:data_path]            = "#{node[:gem_server][:home_path]}/data"
+default[:gem_server][:rubygems_proxy]       = false
+default[:gem_server][:allow_remote_failure] = false
