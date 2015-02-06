7e8d01217e1031f6eea94ec6c607bf33a6846f96
Jamie Winsor <jamie@vialstudios.com>
2015-02-06 15:31:57 -0800

    version bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jamie@vialstudios.com'
 license          'Apache 2.0'
 description      'Installs and configures a geminabox rubygems server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
 
 supports 'ubuntu'
 
